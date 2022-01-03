59017c2d9c875520ff7f59d53b3b3163edd9a93f
max <dumerat.maxime@gmail.com>
2022-01-03 19:13:19 +0100

    Update discord.py

--- discord.py
+++ discord.py
@@ -4,7 +4,6 @@ import discord
 
 TOKEN = os.environ['TOKEN']
 
-
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
@@ -15,7 +14,6 @@ class MyClient(discord.Client):
         if guild.system_channel is not None:
             to_send = f'Welcome {member.mention} to {guild.name}!'
             await guild.system_channel.send(to_send)
-
 
 intents = discord.Intents.default()
 intents.members = True
